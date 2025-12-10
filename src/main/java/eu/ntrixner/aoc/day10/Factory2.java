3e4aaeb4a4da82e3f81a9a843b9f372ef4329377
ntrixner <nikolaus.trixner@cloudflight.io>
2025-12-10 11:54:21 +0100

    Refactorings

--- src/main/java/eu/ntrixner/aoc/day10/Factory2.java
+++ src/main/java/eu/ntrixner/aoc/day10/Factory2.java
@@ -13,12 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Slf4j
 public class Factory2 implements ChallengeRunner<String> {
@@ -27,6 +24,7 @@ public class Factory2 implements ChallengeRunner<String> {
     Pattern curlyPattern = Pattern.compile("\\{(.*?)\\}");
 
     private String input;
+
     @Override
     public void init(String params) {
         this.input = params;
@@ -34,7 +32,7 @@ public class Factory2 implements ChallengeRunner<String> {
 
     @Override
     public void run() {
-        String[] machines =  input.split("\n");
+        String[] machines = input.split("\n");
 
         int totalMinimalComboSize = Arrays.stream(machines)
                 .parallel()
@@ -72,21 +70,21 @@ public class Factory2 implements ChallengeRunner<String> {
     }
 
     @Data
-    public class Machine {
+    public static class Machine {
         boolean[] indicators;
         int[][] buttons;
         int[] joltages; //probably used in part 2
 
         Machine(String indicatorText, String[] buttonsTexts, String joltagesText) {
             indicators = new boolean[indicatorText.length()];
-            for(int i = 0; i < indicatorText.length(); i++){
+            for (int i = 0; i < indicatorText.length(); i++) {
                 indicators[i] = indicatorText.toCharArray()[i] == '#';
             }
             buttons = new int[buttonsTexts.length][indicators.length];
-            for(int i = 0; i < buttonsTexts.length; i++){
+            for (int i = 0; i < buttonsTexts.length; i++) {
                 String button = buttonsTexts[i];
                 List<Integer> indizes = Arrays.stream(button.split(",")).map(Integer::parseInt).toList();
-                for(int j = 0; j < buttons[i].length; j++){
+                for (int j = 0; j < buttons[i].length; j++) {
                     buttons[i][j] = indizes.contains(j) ? 1 : 0;
                 }
             }
@@ -97,23 +95,23 @@ public class Factory2 implements ChallengeRunner<String> {
             Loader.loadNativeLibraries();
             CpModel cpModel = new CpModel();
             int numOfButtons = buttons.length;
-            int numCircuits =  joltages.length;
+            int numCircuits = joltages.length;
 
             //Upper bound - total of all joltages
             int maxPressesPerButton = Arrays.stream(joltages).sum();
 
             // Decision variables - number of times button is pressed
             IntVar[] presses = new IntVar[numOfButtons];
-            for(int i = 0; i < numOfButtons; i++){
+            for (int i = 0; i < numOfButtons; i++) {
                 presses[i] = cpModel.newIntVar(0, maxPressesPerButton, "button_" + i);
             }
 
             //Constraints - sum of button presses per target joltage
-            for(int i = 0; i < numCircuits; i++){
+            for (int i = 0; i < numCircuits; i++) {
                 LinearExprBuilder sum = LinearExpr.newBuilder();
-                for(int b = 0; b < numOfButtons; b++){
+                for (int b = 0; b < numOfButtons; b++) {
                     int delta = buttons[b][i];
-                    if(delta != 0){
+                    if (delta != 0) {
                         sum.addTerm(presses[b], delta);
                     }
                 }
@@ -130,12 +128,11 @@ public class Factory2 implements ChallengeRunner<String> {
 
             CpSolverStatus status = solver.solve(cpModel);
 
-            if(status == CpSolverStatus.OPTIMAL || status == CpSolverStatus.FEASIBLE) {
+            if (status == CpSolverStatus.OPTIMAL || status == CpSolverStatus.FEASIBLE) {
                 int minPresses = (int) solver.value(totalPresses);
                 log.info("One machine done with {} button presses", minPresses);
                 return minPresses;
-            }
-            else {
+            } else {
                 log.warn("machine didn't solve optimally or feasibly");
                 return -1;
             }
